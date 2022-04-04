f367cedd1b0350dac160ebe6216cf197eba308e7
Gregory <gregory.ovis@gmail.com>
2022-04-03 20:30:43 -0400

    changed logo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -50,7 +50,7 @@ const Navbar = (props) => {
         <div className='gpt3__navbar'>
             <div className='gpt3__navbar-links'>
                 <div className='gpt3__navbar-links_logo'>
-                    <img src={logo} alt="logo" />
+                    <h1>Greg's Website</h1> 
                 </div>
                 <div className='gpt3__navbar-links_container'>
                     <Menu />
